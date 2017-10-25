123752c0df138951e6e6c8dc282c499a9a5a2c9b
atiqahhhamzah <atiqahamzah@gmail.com>
2017-10-25 15:35:00 -0500

    Adding python files

---
+++ geo_grid.py
@@ -0,0 +1,68 @@
+import sys
+import numpy as np
+from math import ceil,floor,sqrt
+from pyhdf.SD import *
+
+
+pathname = "/gpfs/largeblockFS/projects/TDataFus/gyzhao/TF/data/MISR/AGP/"
+agp_file = "MISR_AM1_AGP_"
+
+
+def get_latitude_longitude(filename):
+	""" 
+	Gets MISR agp hdf file and
+	outputs the geolatitude and 
+	geolongitude datafields in a 
+	4-d numpy array
+	"""
+
+	# agp_file  = pathname + "MISR_AM1_AGP_" + (filename.replace('MISR_AM1_GRP_ELLIPSOID_GM_',''))[:4] + "_F01_24.hdf"
+
+	# hdf = SD(agp_file)
+
+	hdf = SD(filename)
+
+	lat = hdf.select('GeoLatitude')[:,:,:]
+
+	lon = hdf.select('GeoLongitude')[:,:,:]
+
+	
+	array = np.asarray([lat,lon])
+
+	return array
+
+def get_modis_latitude_longitude(modis_filename):
+	hdf = SD(modis_filename)
+	lat = hdf.select['Latitude']
+	lon = hdf.select['Longitude']
+	return (lat,lon)
+
+def build_agp_dataset(agp_file,exp):
+	"""
+	Takes in MISR agp hdf file 
+	and grid decimal point number 
+	and returns a 2 x 180 x 128 x 512 
+	numpy array that is [longitude, latitude] 
+	x block x width x height
+	"""
+
+	#if filetype == 'MISR':
+	
+	# Obtain coordinate dataset from agp file
+	geo_array = get_latitude_longitude(agp_file)
+
+	# Separate into latitude and longitude files 
+	# and linearize all blocks into a 1-d array
+	lat = geo_array[0].ravel()
+	lon = geo_array[1].ravel() 
+
+	# Build a pixel to latitude,longitude map  
+	lat_lon = np.squeeze(np.dstack((lon,lat)))
+
+	lat_lon = np.floor(np.float64(lat_lon/exp))*exp
+	
+	return lat_lon
+
+
+
+
---
+++ grid_block.py
@@ -0,0 +1,288 @@
+"""
+@author : Nurul Atiqah Hamzah
+@email : hamzah3@illinois.edu
+
+Fixes Needed as of week of 8/4/2017:
+Adapt to Terra Data Fusion blocks and for 
+both MISR and MODIS. Currently works fine
+with misr but not MODIS due to resolution
+differences between MODIS's geolocation 
+attributes and radiance data
+
+Other issues:
+Still trying to figure out how to do a 
+map projection on basemap with dataframe
+radiance data
+
+"""
+
+from math import ceil,floor,sqrt
+import pandas as pd
+import numpy as np
+from pyhdf.SD import *
+import geo_grid
+
+class GridBlock:
+
+
+    def __init__(self,grid_size,filetype='MISR'):
+        """
+        Initializes and computes the class attributes described
+        Input       --  grid_size : Desired length of grid
+        Attributes  --  grid_size : Initializer grid length
+                    --  exp : number of decimal points in grid
+                    --  interval : pixel side length in coordinate degrees
+                    --  width : grid width in total pixels
+                    --  height : grid height in total pixels
+                    --  grid_width : total number of grid columns
+                    --  grid_height : total number of grid rows
+                    --  grid : pandas dataframe that will hold grid data
+                    --  geolocation : pandas dataframa that holds geolocation 
+                        data (specific to MISR)
+        """
+        self.grid_size = grid_size
+        self.width = int(360/grid_size)
+        self.height = int(180/grid_size)
+        self.filetype = filetype
+        self.geolocation = pd.DataFrame()
+        self.grid = pd.DataFrame()
+        self.grid['X'] = np.nan
+        self.grid['Y'] = np.nan
+        self.grid['ij'] = np.nan
+        self.grid['Red'] = np.nan
+        self.grid['Green'] = np.nan
+        self.grid['Blue'] = np.nan
+        self.grid['Year'] = np.nan
+        self.grid['Month'] = np.nan
+        self.grid['Day'] = np.nan
+        if self.filetype == 'MISR':
+            self.grid['Path'] = np.nan
+            self.grid['Orbit'] = np.nan
+        if self.filetype == 'MODIS':
+            self.grid['SolarZenith'] = np.nan
+            self.grid['reflectance_offsets'] = np.nan
+            self.grid['radiance_offsets'] = np.nan
+            self.grid['reflectance_scales'] = np.nan
+
+
+    def init_geolocation_database(self):
+        """
+        Initializes the geolocation dataframe iff MISR
+        is the filetype since MISRs geolocation data is
+        path specific
+        """
+        if self.filetype == 'MISR':
+            self.geolocation['Path'] = np.nan
+            self.geolocation['Latitude'] = np.nan
+            self.geolocation['Longitude'] = np.nan
+            self.geolocation['ij'] = np.nan
+
+
+    def save_grid(self,filename):
+        """
+        Saves the grid dataframe into a pickle
+        file which retains the dataframe structure
+        Input       -- filename : desired file name
+        Output      -- file in current directory
+                       containing dataframe
+        """
+        self.grid.to_pickle(filename.strip() + '.pkl')
+        print 'Saved to ' + filename.strip() + '.pkl'
+    
+
+    def load_grid(self,filename):
+        """
+        Loads the existing grid dataframe
+        from another pkl file
+
+        """
+        self.grid = pd.read_pickle(filename)
+
+
+    def save_geolocation_db(self,filename):
+        """
+        Save the geolocation database in the gridblock
+        to a pkl file
+        """
+        self.geolocation.to_pickle(filename.strip() + '.pkl')
+    
+
+    def load_geolocation_db(self,filename):
+        """
+        Load the geolocation database from another pkl file
+        into the current grid
+        """
+        self.geolocation = pd.read_pickle(filename)
+        
+
+    def insert_geolocation(self,path,latitude,longitude):
+        """
+        Load path specific latitude and longitude data
+        into the geolocation database
+
+        Input       --  path : path associated to latitude 
+                               and longitude data
+                    --  latitude : lat array loaded into db
+                    --  longitude: lon array loaded into db
+        """
+        if any(self.geolocation.Path == path) == True:
+            print "Geolocation information already in database"
+            return
+        else:
+            ij = np.arange(len(latitude))
+            df = pd.DataFrame({ 'Path':pd.Series(path,index=list(range(len(latitude))),dtype='int32'),
+                                'Latitude':latitude,
+                                'Longitude': longitude ,
+                                'ij':ij}) 
+            self.geolocation = self.geolocation.append(df)
+            return
+
+
+    def insert_misr_geolocation(self, agp_file):
+        #Extract path data from file name:
+        path = int(agp_file[agp_file.find('_P') + 2: agp_file.find('_P') + 5])
+        if any(self.geolocation.Path == path) == True:
+            print "Geolocation information already in database"
+            return
+        lat_lon_arr = geo_grid.build_agp_dataset(agp_file,self.grid_size)
+
+        ij = np.arange(len(lat_lon_arr))
+        df = pd.DataFrame({ 'Path':     pd.Series(path,index=list(range(len(ij))),dtype='int32'),
+                            'Latitude': lat_lon_arr[:,1],
+                            'Longitude':lat_lon_arr[:,0],
+                            'ij':ij})
+        self.geolocation = self.geolocation.append(df)
+        
+
+    def describe(self):
+        """
+        Prints out the attributes of the GridBlock
+        Object
+        """
+        print '\nFile type: ' + self.filetype
+        print 'Grid Size in degrees: ' + str(self.grid_size)
+        print 'Width: ' + str(self.width) + ' px'
+        print 'Height: ' + str(self.height) + ' px'
+        print 'Grid Columns: ' + str(list(self.grid.columns.values))
+        print 'Current grid count: ' + str(self.grid.shape[0]) + '\n'
+    
+
+    def insert(self,latitude,longitude, position, red = np.nan, green = np.nan, blue = np.nan, 
+        solar_zenith = np.nan, path = np.nan, orbit = np.nan, year = np.nan, month = np.nan, 
+        day = np.nan, reflectance_scales = np.nan, reflectance_offsets = np.nan,
+        radiance_offsets = np.nan):
+        """ 
+        Appends dataframe with data passed in
+        Make sure to have data be related by row index
+        or discrepancies will occur
+        Input       --  longitude : longitude of pixel (the x coordinate)
+                    --  latitude  : latitude of pixels (the y coordinate)
+                    --  red : red intensity value (16-bit) of pixel
+                    --  green : green intensity value (16-bit) of pixel
+                    --  blue : blue intensity value (16-bit) of pixel
+                    --  path : path number of granule data passed in
+                    --  orbit : orbit number of granule data passed in
+                    --  year : year of granule data passed in
+                    --  month : month of granule data passed in
+                    --  day : day of granule data (if applicable)
+                    --  reflectance_scales : MODIS brf conversion requirement
+                    --  reflectance_offsets : MODIS brf conversion requirement
+                    --  radiance_scales : MODIS brf conversion requirement
+
+        """
+        #grid_array = np.int_(np.ceil(latitude/np.float(self.grid_size)))*np.int_(self.grid_width) + np.int_(np.ceil(longitude/np.float_(self.grid_size)))
+        if self.filetype == 'MISR':
+            dataset = self.geolocation.loc[self.geolocation['Path'] == path]
+            lat_lon = np.squeeze(dataset.as_matrix(['Latitude','Longitude']))
+            lat = lat_lon[:,0]
+            lon = lat_lon[:,1]
+            dataset = pd.DataFrame({ 'X': lon,
+                            'Y': lat,
+                            'ij':position,
+                            'Red': red,
+                            'Green': green,
+                            'Blue' : blue,
+                            'Path':pd.Series(path,index=list(range(len(red))),dtype='float32'),
+                            'Orbit':np.nan,
+                            'Year': year,
+                            'Month': month,
+                            'Day': day})
+        else: 
+            lat = latitude
+            lon = longitude
+            dataset = pd.DataFrame({ 'X': lon,
+                            'Y': lat,
+                            'ij':position,
+                            'Red': red,
+                            'Green': green,
+                            'Blue' : blue,
+                            'Path':pd.Series(path,index=list(range(len(red))),dtype='float32'),
+                            'Orbit':np.nan,
+                            'Year': year,
+                            'Month': month,
+                            'Day': day,
+                            'SolarZenith': solar_zenith,
+                            'reflectance_offsets': pd.Series(reflectance_offsets,index=list(range(len(red))),dtype='float32'),
+                            'radiance_offsets': pd.Series(radiance_offsets,index=list(range(len(red))),dtype='float32'),
+                            'reflectance_scales': pd.Series(reflectance_scales, index=list(range(len(red))),dtype='float32')})
+        self.grid = self.grid.append(dataset)
+      
+
+    def aggr_position(self):
+        """
+        Do an averaging out by pixel location and path
+        """
+        self.grid = self.grid.groupby(['ij','Path'],as_index=False).mean()
+    
+
+    def aggr_custom (self, items):
+        """
+        Average by the column items passed as items
+        Input:    --    items: A list of strings denoting which columns to group by
+        """
+        self.grid = self.grid.groupby(items,as_index=False).mean()
+
+
+    def aggr(self):
+        """
+        Averages and groups data similar in X, Y columns
+        into a single index in the dataframe
+        """
+        self.grid = self.grid.groupby(['X', 'Y'], as_index=False).mean()
+
+
+    def sort(self):
+        """
+        Sorts the dataframe by the X,Y column
+        """
+        return self.grid.sort_values(['X', 'Y'])
+
+    def clear_empty(self):
+        self.grid = self.grid[(self.grid['Red'] != 0) & (self.grid['Green'] != 0) & (self.grid['Blue'] != 0)]
+        return self.grid
+
+
+    def clear_data(self):
+        """
+        Clears all the data inside the dataframe
+        """
+        self.grid = self.grid.drop(self.grid.index[:self.grid.shape[0]])
+
+
+    def get_latitude_longitude(self):
+        """
+        Returns a list of all X and Y values
+        as a numpy array of total index x 2
+        Dim 0 being longitude
+        Dim 1 being latitude
+        """
+        return self.grid.as_matrix(['X','Y'])
+
+
+    def get_radiance_values(self):
+        return self.grid.as_matrix(['Red','Green','Blue'])
+
+    
+    def get_grid_data(self,grid_num):
+        return self.grid[self.grid['Grid Number'] == grid_num]
+
